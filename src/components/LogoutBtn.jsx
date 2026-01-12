d6e8c9009a810fb38bd738e91f37730bb979560c
maluki65 <brayanmaluki@gmail.com>
2026-01-12 16:20:52 +0300

    Resolve build error again

--- src/components/LogoutBtn.jsx
+++ src/components/LogoutBtn.jsx
@@ -1,5 +1,5 @@
 import React from  'react';
-import { useLogout } from '../hooks/useLogout';
+import { useLogout } from '../Hooks/useLogout';
 import { IoIosLogOut } from "react-icons/io";
 
 const LogoutBtn = () => {
