c35c2b81783bfd95b352fc825106aeae4dec56fc
maluki65 <brayanmaluki@gmail.com>
2026-06-29 13:31:52 +0300

    On order invoice

--- src/components/BuyerTabs/BuyerComponents/OrderModal.jsx
+++ src/components/BuyerTabs/BuyerComponents/OrderModal.jsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { RiCloseCircleLine } from "react-icons/ri";
 import { motion, AnimatePresence } from 'framer-motion';
 import { FiPrinter } from "react-icons/fi";
 import { FaCheckCircle, FaCircle, FaBox, FaTruck, FaHome, FaTimesCircle } from "react-icons/fa";
 import { addDays, format } from 'date-fns';
 import { MdOutlineRemoveShoppingCart } from "react-icons/md";
+import { IoLocationOutline } from "react-icons/io5";
+import { useReactToPrint } from 'react-to-print';
 
 function OrderModal({ isOpen, onClose, order, onTrack, onInvoice, type }) {
 
@@ -17,6 +19,13 @@ function OrderModal({ isOpen, onClose, order, onTrack, onInvoice, type }) {
       document.body.style.overflow = '';
     };
   }, [isOpen]);
+
+  const printRef = useRef(null);
+
+  const handlePrint = useReactToPrint({
+    contentRef: printRef,
+    documentTitle: `Order-${order?.orderNumber}`,
+  });
 
   if (!isOpen || !order) return null;
 
@@ -59,7 +68,7 @@ function OrderModal({ isOpen, onClose, order, onTrack, onInvoice, type }) {
       icon: <FaHome/>,
     },
   ];
-
+ 
   return (
     <div 
       onClick={onClose}
@@ -71,7 +80,7 @@ function OrderModal({ isOpen, onClose, order, onTrack, onInvoice, type }) {
         exit={{ opacity: 0, y: -20 }}
         transition={{ duration: 0.3 }}
         onClick={(e) => e.stopPropagation()}
-        className='bg-gray-100 rounded-2xl w-full max-w-xl p-6 shadow-xl m-2'>
+        className='bg-gray-100 rounded-2xl w-full max-w-xl p-6 shadow-xl m-2 orderTrackingWhite'>
           <div className='flex justify-end items-center mb-2'>
             {/*<h2 className='font-semibold text-lg capitalize'>
               {type} Order
@@ -79,7 +88,7 @@ function OrderModal({ isOpen, onClose, order, onTrack, onInvoice, type }) {
             <button onClick={onClose}>
               <RiCloseCircleLine
                 size={28}
-                className='text-red-600 cursor-pointer'
+                className='text-red-600 cursor-pointer trackingIcon'
               />
             </button>
           </div>
@@ -98,12 +107,14 @@ function OrderModal({ isOpen, onClose, order, onTrack, onInvoice, type }) {
           ): (
             <>
               {type === 'track' && (
-                <div className='h-[70vh] overflow-y-auto flex flex-col space-y-2'>
+                <div 
+                  ref={printRef}
+                  className='h-[70vh] overflow-y-auto flex flex-col space-y-2 px-2'>
                   <div className='flex flex-col items-center justify-center gap-2'>
-                  <h1 className='font-semibold text-dark text-center text-lg'>Track Your Order</h1>
-                  <p className='text-gray-500 text-sm'>Stay updated on your order's delivery status in real time.</p>
+                    <h1 className='font-semibold text-dark text-center text-lg trackingTitle'>Track Your Order</h1>
+                    <p className='text-gray-500 text-sm'>Stay updated on your order's delivery status in real time.</p>
                   </div>
-                  <div className='bg-white rounded-md p-4 flex flex-col gap-1'>
+                  <div className='bg-white rounded-md p-4 flex flex-col gap-1 border-[1.5px] border-gray-300'>
                     <div className='flex items-center justify-between TrackOrderNumbers'>
                       <p className='text-dark font-semibold flex items-center gap-2'>
                         Order {order?.orderNumber}
@@ -112,7 +123,8 @@ function OrderModal({ isOpen, onClose, order, onTrack, onInvoice, type }) {
                         </span>
                       </p>
                       <button
-                        className='p-1 border-[1.4px] border-gray-400 flex gap-2 items-center text-dark cursor-pointer rounded font-semibold text-sm hover:text-primary hover:border-primary TrackOrderPrinter'>
+                        onClick={handlePrint}
+                        className='p-1 border-[1.4px] border-gray-400 flex gap-2 items-center text-dark cursor-pointer rounded font-semibold text-sm hover:text-primary hover:border-primary TrackOrderPrinter no-print'>
                           <FiPrinter className='' size={20} />
                           Print
                       </button>
@@ -125,9 +137,9 @@ function OrderModal({ isOpen, onClose, order, onTrack, onInvoice, type }) {
                     })}
                     </p>
                   </div>
-                  <div className='bg-white rounded-md p-5 flex flex-col gap-1'>
+                  <div className='bg-white rounded-md p-5 flex flex-col shadow-sm gap-1 border-[1.5px] border-gray-300 no-print'>
                     <div className='mb-8'>
-                      <h3 className='font-semibold text-lg'>
+                      <h3 className='font-semibold text-md'>
                         Delivery Progress
                       </h3>
 
@@ -183,6 +195,7 @@ function OrderModal({ isOpen, onClose, order, onTrack, onInvoice, type }) {
                           </div>
                         ))}
                       </div>
+
                     </div>
 
                     {/*On small screen*/}
@@ -232,6 +245,82 @@ function OrderModal({ isOpen, onClose, order, onTrack, onInvoice, type }) {
                             </div>
                         </div>
                       ))}
+                    </div>
+                  </div>
+
+                  <div className='bg-white rounded-md p-5 flex flex-col gap-2 shadow-sm border-[1.5px] border-gray-300'>
+                    <h1 className='font-semibold text-md'>
+                      Order Items
+                    </h1>
+                    <div className='flex flex-col gap-2'>
+                      {order?.products.map((product) => {
+                        return (
+                          <>
+                            <div 
+                              key={product?._id}
+                              className='flex items-center justify-between orderProductModal'
+                              >
+                                <div className='flex items-center gap-2'>
+                                  <img
+                                    src={product?.image}
+                                    alt={product?.name}
+                                    className='h-15 w-17 rounded-md object-contain no-print'
+                                    loading='lazy'
+                                  />
+                                  <div className='flex flex-col gap-1'>
+                                    <h3 className='text-dark font-semibold'>
+                                      {product?.name}
+                                    </h3>
+                                    <p className='text-gray-500 text-sm'>
+                                      Qty: {product?.quantity}
+                                    </p>
+                                  </div>
+                                </div>
+                                <div className='flex flex-col gap-2 items-end productOrderPrice'>
+                                  <h3 className='text-dark font-semibold'>
+                                    ksh{(product?.price / 100).toLocaleString()}
+                                  </h3>
+                                  <p className='text-gray-500 text-xs'>
+                                  ksh {(product?.price / 100).toLocaleString()} total
+                                  </p>
+                                </div>
+                            </div>
+                            <hr className='flex-1 border-t border-gray-300' />                            
+                          </>
+                        )
+                      })}
+                    </div>
+                    <div className='my-1 flex items-center justify-between'>
+                      <p className='text-sm text-gray-500'>{order?.productCount} item(s)</p>
+                      <p className='text-sm text-gray-500'>Total: ksh{(order?.totalAmount / 100).toLocaleString()}</p>
+                    </div>
+                  </div>
+
+                  {/*}<div className='grid grid-cols-2 gap-2'>
+                    <div className='bg-white rounded-md px-3 py-2 flex flex-col gap-2 border-[1.5px] border-gray-300'>
+                      <h4 className='font-semibold flex items-center gap-1'>
+                        <IoLocationOutline className='' size={18}/>
+                        Shipping Address
+                      </h4>
+                    </div>
+                  </div>*/}
+                  <div className='bg-white rounded-md p-5 flex flex-col gap-2 shadow-sm border-[1.5px] border-gray-300 justify-center items-center mb-2 no-print'>
+                    <h2 className='font-semibold text-dark text-md text-center'>
+                      Need Help with Your Order?
+                    </h2>
+                    <p className='text-sm text-gray-500 text-center'>
+                      Our support team is here to help you with any questions
+                    </p>
+                    <div className='flex gap-2 items-center orderSupport'>
+                      <button 
+                        className='px-4 py-2 rounded-md text-dark font-semibold border-[1.4px] border-gray-400 hover:bg-gray-200 cursor-pointer'>
+                          Contact Support
+                      </button>
+                      <button 
+                        onClick={onClose}
+                        className='px-4 py-2 rounded-md text-dark font-semibold border-[1.4px] border-gray-400 hover:bg-gray-200 cursor-pointer'>
+                          View Order Details
+                      </button>
                     </div>
                   </div>
                 </div>
