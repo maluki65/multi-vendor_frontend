c299637c4802840316ba390dc359108f1e758342
maluki65 <brayanmaluki@gmail.com>
2026-08-13 13:19:00 +0300

    On store

--- src/components/BuyerTabs/pages/storeInfo.jsx
+++ src/components/BuyerTabs/pages/storeInfo.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import './../BuyerTabs.css';
 import { useParams } from 'react-router-dom';
 import { Inner } from '../../../commons';
-import { AdLoader } from '../../';
+import { cartB8 } from '../../../assets';
+import { AdLoader, Footer } from '../../';
 import useProducts from '../../../Hooks/useProduts';
 import { MdError, MdRemoveShoppingCart } from 'react-icons/md';
 import { RiVerifiedBadgeFill } from 'react-icons/ri';
@@ -20,7 +21,42 @@ function StoreInfo() {
   console.log('Products', products);
   return (
     <Inner>
-      <div>vendorInfo</div>
+      <section className='min-h-[35vh] w-full relative bg-gray-100'
+        style={{
+          backgroundImage: `url(${vendor?.banner || cartB8 })`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat'
+        }}>
+          <div className='absolute bottom-5 right-8 z-10'>
+            <div className='w-28 h-28 rounded-full border-4 border-white overflow-hidden shadow-md bg-white'>
+              <img
+                src={vendor?.logo}
+                alt={vendor?.businessInfo?.legalName}
+                loading='lazy'
+                className='w-full h-full object-cover'
+              />
+            </div>
+          </div>          
+      </section>
+
+      <section className='px-[2%] my-5 overflow-hidden min-h-[30vh]'>
+        <div className='grid grid-cols-[35%_65%] gap-2 items-center'>
+          <h1 className='px-3 py-2 rounded-full border-[1.4px] font-semibold border-dark w-fit m-3 hover:bg-gray-200 hover:text-primary hover:border-orange-400 flex items-center gap-2'>
+            About {vendor?.businessInfo?.legalName}
+            <span className=''>
+              {vendor?.verification?.isverified === true ? (
+                <RiVerifiedBadgeFill className='text-primary' size={23} />
+              ) : (
+                <p className=''/>
+              )}
+            </span>
+          </h1>
+          <p className='text-right px-4 font-medium text-dark tracking-wide'>
+           Fascism is a far-right, authoritarian, and ultranationalist political ideology. It features a dictatorial leader, strict societal control, and the violent crushing of any opposition. The state or race comes before individual rights.
+          </p> {/*{vendor?.store?.description}*/}
+        </div>
+      </section>
     </Inner>
   )
 }
